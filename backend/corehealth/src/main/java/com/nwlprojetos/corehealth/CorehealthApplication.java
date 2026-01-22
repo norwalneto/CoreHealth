9bfc7675d8dffb1d65b63a1375cff50c3465b4df
norwalneto <norwalgomess@gmail.com>
2026-01-22 12:16:35 -0300

    arquivos iniciais

---
+++ backend/corehealth/src/main/java/com/nwlprojetos/corehealth/CorehealthApplication.java
@@ -0,0 +1,13 @@
+package com.nwlprojetos.corehealth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorehealthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CorehealthApplication.class, args);
+	}
+
+}
---
+++ backend/corehealth/src/test/java/com/nwlprojetos/corehealth/CorehealthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nwlprojetos.corehealth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CorehealthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
